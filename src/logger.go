e68045cff38fb1a4cbc08387520fba299f862fd5
Pavel Pushkarev <riarheos@gmail.com>
2025-01-28 04:39:59 +0300

    Cleaner log so that systemd doesn't complain about tabs

--- src/logger.go
+++ src/logger.go
@@ -1,9 +1,23 @@
 package src
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 func createLogger(verbose bool) *zap.Logger {
-	cfg := zap.NewDevelopmentConfig()
+	cfg := zap.Config{
+		Encoding:    "console",
+		OutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:       "message",
+			LevelKey:         "level",
+			EncodeLevel:      zapcore.CapitalLevelEncoder,
+			ConsoleSeparator: "  ",
+		},
+		Level: zap.NewAtomicLevelAt(zap.DebugLevel),
+	}
+
 	if !verbose {
 		cfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
 	}
